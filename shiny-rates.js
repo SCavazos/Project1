d8f2936c4213ebf2021eb96a44b15533201e940f
Sara <amystery27@gmail.com>
2020-07-15 21:00:51 -0400

    bootstrap navbar

--- shiny-rates.js
+++ shiny-rates.js
@@ -1,2 +1,10 @@
-const bulba_seen = document.getElementById (bulba_seen);
-console.log(bulba_seen);
+function storeBulbaInfo() {
+    let bulba_see = document.getElementById("bulba_seen").value;
+    let bulba_shine = document.getElementById("bulba_shiny").value;
+}
+
+let button = document.getElementById("bulba_rate");
+button.addEventListener("click", storeBulbaInfo);
+
+console.log(bulba_see);
+console.log(bulba_shine);
