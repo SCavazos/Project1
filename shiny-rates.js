e5f91848be2ea0ab63a046ec4beaddc49561498f
Sara <amystery27@gmail.com>
2020-07-17 12:39:34 -0400

    navigation fix and attempt at JS

--- shiny-rates.js
+++ shiny-rates.js
@@ -1,10 +1,8 @@
 function storeBulbaInfo() {
-    let bulba_see = document.getElementById("bulba_seen").value;
-    let bulba_shine = document.getElementById("bulba_shiny").value;
+    const bulba_seen = document.getElementById("bulba_seen").value;
+    const bulba_shiny = document.getElementById("bulba_shiny").value;
 }
 
-let button = document.getElementById("bulba_rate");
+const button = document.getElementById("bulba_rate");
 button.addEventListener("click", storeBulbaInfo);
 
-console.log(bulba_see);
-console.log(bulba_shine);
