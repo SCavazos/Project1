68c05de4c0cbd5f550bc5d95b0207293d3af7717
Sara <amystery27@gmail.com>
2020-07-18 12:57:40 -0400

    completed JS

--- shiny-rates.js
+++ shiny-rates.js
@@ -1,8 +1,10 @@
-function storeBulbaInfo() {
-    const bulba_seen = document.getElementById("bulba_seen").value;
-    const bulba_shiny = document.getElementById("bulba_shiny").value;
+function storePokemonInfo() {
+    const pokemon_seen = document.getElementById("pokemon_seen").value;
+    const pokemon_shiny = document.getElementById("pokemon_shiny").value;
+    const pokemon_shiny_rate = parseInt(pokemon_shiny) / parseInt(pokemon_seen);
+    document.getElementById("result").innerHTML = (`Your shiny rate is ${pokemon_shiny_rate}`);
 }
 
-const button = document.getElementById("bulba_rate");
-button.addEventListener("click", storeBulbaInfo);
+const button = document.getElementById("pokemon_rate");
+button.addEventListener("click", storePokemonInfo);
 
