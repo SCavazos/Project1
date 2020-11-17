1eb31cafa63ea0745d2ded030d7861e6968ae646
Sara Cavazos <amystery27@gmail.com>
2020-11-16 22:01:53 -0500

    check password using regex

--- js/jsProject2.js
+++ js/jsProject2.js
@@ -1,11 +1,14 @@
 let pokemonList
 
+// Test Function
+
 function handleClick(event) {
 
     console.log(event.target.alt);
-   
-
+    console.log(event.target.dataset.url);
 };
+
+// API Request and Callback Function
 
 fetch('https://pokeapi.co/api/v2/pokedex/kanto')
   .then(response => response.json())
@@ -15,12 +18,10 @@ fetch('https://pokeapi.co/api/v2/pokedex/kanto')
     data.pokemon_entries.forEach(pokemon => { 
         const name = pokemon.pokemon_species.name;
         const url = pokemon.pokemon_species.url;
-        console.log("url", url);
-        pokemonList = pokemonList + `<a><img src= "https://img.pokemondb.net/sprites/black-white/normal/${name}.png" alt= "${name}" onclick="handleClick(event)"></a>`
+        pokemonList = pokemonList + `<img src= "https://img.pokemondb.net/sprites/black-white/normal/${name}.png" alt= "${name}" onclick="handleClick(event)" data-url="${url}">`
 
     })
 
-    
     document.getElementById("kantoPokemon").innerHTML = pokemonList;
     });
 
---
+++ js/passwordValidator.js
@@ -0,0 +1,27 @@
+//-------------- stores input ------------------
+//-------------- checks to see if requirement is met ------------------
+//-------------- displays message if requirement is missing ------------------
+
+function validatePassword() {
+    const passwordInput = document.getElementById("password").value;
+    
+    const hasUpperCase = /[A-Z]/.test(passwordInput);
+    const hasNumber = /[0-9]/.test(passwordInput);
+    const hasCharacter = /[!@#$%^&*(),.?":{}|<>]/.test(passwordInput);
+
+    if (passwordInput.length != 8) {
+        document.getElementById("missing_requirement").innerHTML = (`Your password must be 8 characters long.`)
+    } else if (!hasUpperCase) {
+        document.getElementById("missing_requirement").innerHTML = (`Your password must contain an upper case letter.`)
+    } else if (!hasNumber) {
+        document.getElementById("missing_requirement").innerHTML = (`Your password must contain a number.`)
+    } else if (!hasCharacter) {
+        document.getElementById("missing_requirement").innerHTML = (`Your password must contain a special character.`)
+    } else;
+};
+const submit = document.getElementById("submit");
+submit.addEventListener("click", validatePassword);
+
+
+//  this regex checks that password is at least 8 characters long, has a number, upper case letter, and character 
+// /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(passwordInput);
