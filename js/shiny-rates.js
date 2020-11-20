f6a0711d7d1e8ce8c1b39de24717e537534ea77b
Sara Cavazos <amystery27@gmail.com>
2020-11-20 11:48:39 -0500

    reverts shiny rates js

--- js/shiny-rates.js
+++ js/shiny-rates.js
@@ -1,12 +1,16 @@
-let seen = document.getElementById("pokemon_seen");
-let shiny = document.getElementById("pokemon_shiny");
-let pokemon_shiny_rate = "";
+
+
+
+function storePokemonInfo() {
+
+    let seen = document.getElementById("pokemon_seen").value;
+    let shiny = document.getElementById("pokemon_shiny").value;
+    let pokemon_shiny_rate = (parseInt(shiny) / parseInt(seen)) * 100;
+    document.getElementById("result").innerHTML = (`Your shiny rate is ${pokemon_shiny_rate} %`);
+
+};
+
+
 
 const button = document.getElementById("pokemon_rate");
-button.addEventListener("click", function storePokemonInfo(seen, shiny) {
-
-    pokemon_shiny_rate = parseInt(shiny) / parseInt(seen) * 100;
-
-    document.getElementById("result").innerHTML = (`Your shiny rate is ${pokemon_shiny_rate} %`);
-});
-
+button.addEventListener("click", storePokemonInfo);
